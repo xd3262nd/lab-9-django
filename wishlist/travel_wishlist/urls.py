9e599ae776c689ffa51c33534c5053b370283731
qian587 <tanxx587@umn.edu>
2020-11-05 13:58:40 -0500

    added about page

--- wishlist/travel_wishlist/urls.py
+++ wishlist/travel_wishlist/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.place_list, name='place_list'),
+    path('about', views.about, name='about'),
     path('visited', views.places_visited, name='places_visited'),
     path('place/<int:place_pk>/was_visited/', views.place_was_visited, name='place_was_visited')
 ]
--- wishlist/travel_wishlist/views.py
+++ wishlist/travel_wishlist/views.py
@@ -12,20 +12,24 @@ def place_list(request):
         a list of places and a form to add a new place.
     """
     if request.method == 'POST':
-        form = NewPlaceForm(request.POST)
-        place = form.save()  # Create a new Place from the form
+        form = NewPlaceForm(request.POST) # this is to create a form object based on what is being sent from the server
+        place = form.save()  # Create a new Place from the form or creating a model object from the form
         if form.is_valid():  # Checks against DB Constraints, for example, are required fields present?
-            place.save()  # Saves to the database
-            return redirect('place_list')  # redirects to GET view with name place_list - which is this same view
+            place.save()  # Saves places to the database
+            return redirect('place_list')  # redirects to GET view with name place_list - which is this same view or reload the homepage
 
     # If not a POST, or the form is not valid, render the page
     # with the form to add a new place, and list of places
     places = Places.objects.filter(visited=False).order_by('name')
-    new_place_form = NewPlaceForm()
+    new_place_form = NewPlaceForm() # used to create the HTML
 
     return render(request, 'travel_wishlist/wishlist.html', {'places': places, 'new_place_form': new_place_form})
 
+def about(request):
+    author = 'Qian'
+    about = 'A website to create a list of places to visit'
 
+    return render(request, 'travel_wishlist/about.html', { 'author': author, 'about': about})
 
 def places_visited(request):
     visited = Places.objects.filter(visited=True)
